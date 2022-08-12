7085b4cdb69ad687a87b2b4274776cdacd19d14f
Tiber <danilo.ochoa.hidalgo@outlook.com>
2022-08-11 23:56:30 -0500

    Using UI Kitten library

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StyleSheet, Text, View } from 'react-native';
 
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+
+const HomeScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text category='h1'>HOME</Text>
+  </Layout>
+);
+
+export default () => (
+  <ApplicationProvider {...eva} theme={eva.light}>
+    <HomeScreen />
+  </ApplicationProvider>
+);
+
+
+/*
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text>Open up App.tsx to start working on your app!</Text>
+    <StatusBar style="auto" />
     </View>
   );
 }
@@ -18,3 +36,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+*/
